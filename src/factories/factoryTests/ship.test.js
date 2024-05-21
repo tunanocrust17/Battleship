7800aa1c160b0d74fef2f2cb456287ed73eb5c2a
ewhuet <ewhuet@gmail.com>
2024-05-21 06:18:12 +0300

    built tests to add hit method and to check if the ship is sunk then developed the actual methods inside the ship factory

--- src/factories/factoryTests/ship.test.js
+++ src/factories/factoryTests/ship.test.js
@@ -1,13 +1,29 @@
 import { Ship } from "../ships";
 
-let testShip = new Ship("carrier", 5);
-let shipResult = {
+let testBuildShip = new Ship("carrier", 5);
+let shipBuildResult = {
     name: "carrier",
     length: 5,
-    hits: 0,
+    currentDamage: 0,
     isSunk: false
 }
 
 test('creates new ship factory', () => {
-    expect(testShip).toMatchObject(shipResult)
+    expect(testBuildShip).toMatchObject(shipBuildResult)
+})
+
+
+test('hit method increases ship hits', () => {
+    let hitTestShip = new Ship('carrier', 5)
+    for(let i = 0 ; i < 4 ; i++) {
+        hitTestShip.hit()
+    }
+    expect(hitTestShip.currentDamage).toBe(4)
+})
+
+test('ship is sunk', () =>{
+    for( let i = 0 ; i < 5 ; i++){
+        testBuildShip.hit()
+    }
+    expect(testBuildShip.isSunk).toBe(true)
 })
--- src/factories/ships.js
+++ src/factories/ships.js
@@ -2,9 +2,25 @@ class Ship {
     constructor(name, length){
         this.name = name
         this.length = length
-        this.hits = 0
+        this.currentDamage = 0
         this.isSunk = false
     }
+
+    hit(){
+        this.currentDamage++
+        this.sinkShip()
+    }
+
+    sinkShip(){
+        if(this.currentDamage === this.length){
+            this.isSunk = true
+        }
+    }
+
+    checkIfSunk(){
+        return this.isSunk
+    }
 }
+
 
 export {Ship}
