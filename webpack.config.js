177003c4ce2c3f5356fa5d0970e4d42f4d16c380
Eric Huet <ewhuet@gmail.com>
2024-05-29 13:48:10 +0100

    updated to use css-loader and style-loader with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/app.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'To Do',
+      title: 'Battleship',
       icon: "./src/styles/magnify.svg",
       filename: "index.html",
       template: path.resolve(__dirname, "src", "index.html"),
@@ -16,5 +16,17 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/inline',
+      },
+    ],
   },
 };
