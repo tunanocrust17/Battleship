3d56b15190ed17a3531b88479de45eac543e63bb
ewhuet <ewhuet@gmail.com>
2024-06-08 06:40:51 +0300

    - import and created the bot board which is hidden until the game starts

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import './styles.css'
 import { gameController } from './gameController';
+import { createBotBoard } from './factories/components/gameboardUI';
 
+createBotBoard()
 gameController()
+
